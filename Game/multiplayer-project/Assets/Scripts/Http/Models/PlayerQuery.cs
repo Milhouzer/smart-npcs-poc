5f6b7e11c77516d8fcdda32e523c84632656b395
Milhouzer <milan.hernando@yahoo.fr>
2024-11-01 12:11:02 +0100

    feat(http): api client

---
+++ Game/multiplayer-project/Assets/Scripts/Http/Models/PlayerQuery.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Http.Models
+{
+    // PlayerQuery represents the structure for sending a query to the bot
+    public class PlayerQuery
+    {
+        public int id_bot { get; set; }
+        public int id_player { get; set; }
+        public string player_query { get; set; }
+        public Dictionary<string, string> additional_argument { get; set; } = new();
+    }
+
+    // BotAnswer represents the structure for receiving a response from the bot
+    public class BotAnswer
+    {
+        public int id_bot { get; set; }
+        public int id_player { get; set; }
+        public string message { get; set; }
+        public List<string> actions { get; set; } = new();
+    }
+}
