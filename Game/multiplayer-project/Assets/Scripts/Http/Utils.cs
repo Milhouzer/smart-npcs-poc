2f0c9132b45821c524487c73bb582962b8eb0225
Milhouzer <milan.hernando@yahoo.fr>
2024-11-01 12:11:02 +0100

    feat(http): api client

---
+++ Game/multiplayer-project/Assets/Scripts/Http/Utils.cs
@@ -0,0 +1,14 @@
+﻿using Palmmedia.ReportGenerator.Core.Common;
+
+namespace Http
+{
+    public static class Utils
+    {
+        public static T DeserializeJson<T>(string json)
+        {
+            T structure = default(T);
+
+            return structure;
+        }
+    }
+}
