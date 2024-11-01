d25390dbe3a49a4263d9ed05dd4033177d74a93a
LaBrens <maxbreney@gmail.com>
2024-11-01 11:49:58 +0100

    fix api

--- Brechi/src/api/api.py
+++ Brechi/src/api/api.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 from fastapi.responses import JSONResponse
-from src.llm.response import BotAnswer, get_bot_answer, PlayerQuery
+from src.llm.base import BotAnswer, get_bot_answer, PlayerQuery
 
 # Define the app instance
 app = FastAPI()
@@ -11,13 +11,13 @@ app = FastAPI()
 
 @app.get("/test")
 async def test_api_connexion() -> JSONResponse:
-    return JSONResponse(status_code=200)
+    return JSONResponse(status_code=200, content="OK")
 
 
 # CRUD Endpoints
-@app.post("/talk-to-bot", response_model=JSONResponse)
+@app.post("/talk-to-bot")
 async def talk_to_bot(player_query: PlayerQuery) -> JSONResponse:
 
-    bot_answer = get_bot_answer(player_query)
+    bot_answer = "bot answer"  # get_bot_answer(player_query)
 
     return JSONResponse(content=bot_answer)
--- Brechi/src/llm/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_prompt import get_prompt_template
-from .model import get_llm
-from .response import get_parser_output
