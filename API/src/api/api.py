0f3b19c3df35a562aa10f497189235861e3735ec
LaBrens <maxbreney@gmail.com>
2024-11-08 16:57:36 +0100

    fix image docker

--- API/src/api/api.py
+++ API/src/api/api.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from typing import List
 from fastapi.responses import JSONResponse
 
-# from .llm.base import BotAnswer, get_bot_answer, PlayerQuery
+from src.llm.base import BotAnswer, get_bot_answer, PlayerQuery
 
 # Define the app instance
 app = FastAPI()
@@ -15,10 +15,10 @@ async def test_api_connexion() -> JSONResponse:
     return JSONResponse(status_code=200, content=" - ".join(os.getcwd(), os.listdir()))
 
 
-# # CRUD Endpoints
-# @app.post("/talk-to-bot")
-# async def talk_to_bot(player_query: PlayerQuery) -> JSONResponse:
+# CRUD Endpoints
+@app.post("/talk-to-bot")
+async def talk_to_bot(player_query: PlayerQuery) -> JSONResponse:
 
-#     bot_answer = "bot answer"  # get_bot_answer(player_query)
+    bot_answer = "bot answer"  # get_bot_answer(player_query)
 
-#     return JSONResponse(content=bot_answer)
+    return JSONResponse(status_code=200, content=bot_answer)
